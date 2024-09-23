53e7717588619fd3eede522ccf6e277b5778933e
Patryk <patryk.rakowski@cwmlegal.pl>
2024-09-23 11:57:36 +0200

    initial commit

---
+++ wprowadzenie_do_jezyka_csharp_czesc_2/create_and_run_simple_csharp_console_applications/create_and_run_simple_csharp_console_applications/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace create_and_run_simple_csharp_console_applications;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var dice = new Random();
+        var roll = dice.Next(1, 7);
+        Console.WriteLine($"Roll: {roll}");
+        
+        var number = 7;
+        var text = "seven";
+
+        Console.WriteLine();
+        Console.WriteLine(number);
+        Console.WriteLine(text);
+        
+        var roll1 = dice.Next();
+        var roll2 = dice.Next(101);
+        var roll3 = dice.Next(50, 101);
+
+        Console.WriteLine();
+        Console.WriteLine($"First roll: {roll1}");
+        Console.WriteLine($"Second roll: {roll2}");
+        Console.WriteLine($"Third roll: {roll3}");
+    }
+}
