5ca0e0f522eaae46ff61e4a09ba300dbf3af37f8
Patryk <patryk.rakowski@cwmlegal.pl>
2024-09-23 21:05:57 +0200

    Cwiczenie1Challenge.cs

--- dodawanie_logiki_do_aplikacji_jezyka_csharp/dodawanie_logiki_do_aplikacji_jezyka_csharp/Cwiczenie1Challenge.cs
+++ dodawanie_logiki_do_aplikacji_jezyka_csharp/dodawanie_logiki_do_aplikacji_jezyka_csharp/Cwiczenie1Challenge.cs
@@ -5,8 +5,33 @@ public static class Cwiczenie1Challenge
     public static void Run()
     {
         string permission = "Admin|Manager";
-        int level = 55;
-        
-        
+        int level = 53;
+
+        if (permission.Contains("Admin"))
+        {
+            if (level > 55)
+            {
+                Console.WriteLine("Welcome, Super Admin user.");
+            }
+            else
+            {
+                Console.WriteLine("Welcome, Admin user.");
+            }
+        }
+        else if (permission.Contains("Manager"))
+        {
+            if (level >= 20)
+            {
+                Console.WriteLine("Contact an Admin for access.");
+            }
+            else
+            {
+                Console.WriteLine("You do not have sufficient privileges.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("You do not have sufficient privileges.");
+        }
     }
 }
