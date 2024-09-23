00673898a982c9f450b6a02d904b47498bac05ac
Patryk <patryk.rakowski@cwmlegal.pl>
2024-09-23 23:05:01 +0200

    Cwiczenie5Challenge.cs

---
+++ dodawanie_logiki_do_aplikacji_jezyka_csharp/dodawanie_logiki_do_aplikacji_jezyka_csharp/Cwiczenie5Challenge.cs
@@ -0,0 +1,28 @@
+namespace dodawanie_logiki_do_aplikacji_jezyka_csharp;
+
+public static class Cwiczenie5Challenge
+{
+    public static void Run()
+    {
+        int hero = 10;
+        int monster = 10;
+
+        Random dice = new Random();
+
+        do
+        {
+            int roll = dice.Next(1, 11);
+            monster -= roll;
+            Console.WriteLine($"Monster was damaged and lost {roll} health and now has {monster} health.");
+
+            if (monster <= 0) continue;
+
+            roll = dice.Next(1, 11);
+            hero -= roll;
+            Console.WriteLine($"Hero was damaged and lost {roll} health and now has {hero} health.");
+
+        } while (hero > 0 && monster > 0);
+
+        Console.WriteLine(hero > monster ? "Hero wins!" : "Monster wins!");
+    }
+}
