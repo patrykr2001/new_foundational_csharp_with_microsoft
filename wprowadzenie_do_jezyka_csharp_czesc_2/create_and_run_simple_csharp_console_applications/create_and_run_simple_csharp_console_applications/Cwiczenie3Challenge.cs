11f5e1bb564c14d03a52a9381f3789c3ba9adb92
Patryk <patryk.rakowski@cwmlegal.pl>
2024-09-23 12:45:16 +0200

    Cwiczenie3Challenge.cs

--- wprowadzenie_do_jezyka_csharp_czesc_2/create_and_run_simple_csharp_console_applications/create_and_run_simple_csharp_console_applications/Cwiczenie3Challenge.cs
+++ wprowadzenie_do_jezyka_csharp_czesc_2/create_and_run_simple_csharp_console_applications/create_and_run_simple_csharp_console_applications/Cwiczenie3Challenge.cs
@@ -4,46 +4,14 @@ public static class Cwiczenie3Challenge
 {
     public static void Run()
     {
-        // Zad 1
-        /*string[] fraudulentOrderIDs = new string[3];
+        string[] ordersIds = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
 
-        fraudulentOrderIDs[0] = "A123";
-        fraudulentOrderIDs[1] = "B456";
-        fraudulentOrderIDs[2] = "C789";
-        // fraudulentOrderIDs[3] = "D000";*/
-        
-        string[] fraudulentOrderIDs = ["A123", "B456", "C789"];
-
-        Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
-        Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
-        Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
-        
-        fraudulentOrderIDs[0] = "F000";
-
-        Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
-        
-        Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
-        
-        // Zad 2
-        
-        string[] names = { "Rowena", "Robin", "Bao" };
-        
-        foreach (string name in names)
+        foreach (var orderId in ordersIds)
         {
-            Console.WriteLine(name);
+            if (orderId.StartsWith("B"))
+            {
+                Console.WriteLine(orderId);
+            }
         }
-        
-        int[] inventory = [200, 450, 700, 175, 250];
-        int sum = 0;
-        int bin = 0;
-        
-        foreach (int items in inventory)
-        {
-            sum += items;
-            bin++;
-            Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
-        }
-        
-        Console.WriteLine($"We have {sum} items in inventory.");
     }
 }
