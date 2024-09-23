c52ec79d3f207460be831f245aed9f895c929745
Patryk <patryk.rakowski@cwmlegal.pl>
2024-09-23 12:13:49 +0200

    Cwiczenie2Challenge.cs

--- wprowadzenie_do_jezyka_csharp_czesc_2/create_and_run_simple_csharp_console_applications/create_and_run_simple_csharp_console_applications/Cwiczenie2Challenge.cs
+++ wprowadzenie_do_jezyka_csharp_czesc_2/create_and_run_simple_csharp_console_applications/create_and_run_simple_csharp_console_applications/Cwiczenie2Challenge.cs
@@ -4,6 +4,34 @@ public static class Cwiczenie2Challenge
 {
     public static void Run()
     {
+        var random = new Random();
+        var daysUntilExpiration = random.Next(12);
+        var discountPercentage = 0;
+
+        // Your code goes here
+
+        if (daysUntilExpiration <= 0)
+        {
+            Console.WriteLine("Your subscription has expired.");
+        }
+        else if (daysUntilExpiration == 1)
+        {
+            Console.WriteLine("Your subscription expires within a day!");
+            discountPercentage = 20;
+        }
+        else if (daysUntilExpiration <= 5)
+        {
+            Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days!");
+            discountPercentage = 10;
+        }
+        else if (daysUntilExpiration <= 10)
+        {
+            Console.WriteLine("Your subscription will expire soon. Renew now!");
+        }
         
+        if (discountPercentage > 0)
+        {
+            Console.WriteLine($"Renew now and save {discountPercentage}%.");
+        }
     }
 }
