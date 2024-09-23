efaac5b711bf98c6f379a52345ac8624623ffb4c
Patryk <patryk.rakowski@cwmlegal.pl>
2024-09-23 21:01:58 +0200

    Cwiczenie1.cs

--- dodawanie_logiki_do_aplikacji_jezyka_csharp/dodawanie_logiki_do_aplikacji_jezyka_csharp/Cwiczenie1.cs
+++ dodawanie_logiki_do_aplikacji_jezyka_csharp/dodawanie_logiki_do_aplikacji_jezyka_csharp/Cwiczenie1.cs
@@ -4,6 +4,57 @@ public static class Cwiczenie1
 {
     public static void Run()
     {
+        /*Console.WriteLine("a" == "a");
+        Console.WriteLine("a" == "A");
+        Console.WriteLine(1 == 2);
+
+        string myValue = "a";
+        Console.WriteLine(myValue == "a");
         
+        Console.WriteLine("a" == "a ");
+        
+        string value1 = " a";
+        string value2 = "A ";
+        Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());*/
+        
+        /*Console.WriteLine("a" != "a");
+        Console.WriteLine("a" != "A");
+        Console.WriteLine(1 != 2);
+
+        string myValue = "a";
+        Console.WriteLine(myValue != "a");
+        
+        Console.WriteLine(1 > 2);
+        Console.WriteLine(1 < 2);
+        Console.WriteLine(1 >= 1);
+        Console.WriteLine(1 <= 1);
+        
+        string pangram = "The quick brown fox jumps over the lazy dog.";
+        Console.WriteLine(pangram.Contains("fox"));
+        Console.WriteLine(pangram.Contains("cow"));
+        
+        // These two lines of code will create the same output
+
+        Console.WriteLine(pangram.Contains("fox") == false);
+        Console.WriteLine(!pangram.Contains("fox"));
+        
+        Console.WriteLine(!pangram.Contains("fox"));
+        Console.WriteLine(!pangram.Contains("cow"));*/
+        
+        /*int a = 7;
+        int b = 6;
+        Console.WriteLine(a != b); // output: True
+        string s1 = "Hello";
+        string s2 = "Hello";
+        Console.WriteLine(s1 != s2); // output: False*/
+        
+        /*int saleAmount = 1001;
+        // int discount = saleAmount > 1000 ? 100 : 50;
+
+        Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");*/
+        
+        /*Random coin = new Random();
+        int flip = coin.Next(0, 2);
+        Console.WriteLine((flip == 0) ? "heads" : "tails");*/
     }
 }
---
+++ dodawanie_logiki_do_aplikacji_jezyka_csharp/dodawanie_logiki_do_aplikacji_jezyka_csharp/Cwiczenie1Challenge.cs
@@ -0,0 +1,12 @@
+namespace dodawanie_logiki_do_aplikacji_jezyka_csharp;
+
+public static class Cwiczenie1Challenge
+{
+    public static void Run()
+    {
+        string permission = "Admin|Manager";
+        int level = 55;
+        
+        
+    }
+}
