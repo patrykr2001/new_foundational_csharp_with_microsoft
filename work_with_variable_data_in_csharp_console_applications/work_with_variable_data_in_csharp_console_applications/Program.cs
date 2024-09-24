cd0bfa03b29f6beb26a272f05911704b86c84a3c
Patryk <patryk.rakowski@cwmlegal.pl>
2024-09-24 10:38:16 +0200

    work_with_variable_data_in_csharp_console_applications.sln initial commit

---
+++ work_with_variable_data_in_csharp_console_applications/work_with_variable_data_in_csharp_console_applications/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace work_with_variable_data_in_csharp_console_applications;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
