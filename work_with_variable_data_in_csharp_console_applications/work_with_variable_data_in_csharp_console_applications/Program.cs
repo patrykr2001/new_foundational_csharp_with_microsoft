e5c3c130f39aab76060b8a5fd5feb7cf1aefd7da
Patryk <patryk.rakowski@cwmlegal.pl>
2024-10-01 23:48:08 +0200

    Cwiczenie4.cs

--- work_with_variable_data_in_csharp_console_applications/work_with_variable_data_in_csharp_console_applications/Program.cs
+++ work_with_variable_data_in_csharp_console_applications/work_with_variable_data_in_csharp_console_applications/Program.cs
@@ -8,6 +8,7 @@ class Program
         //Cwiczenie2.Run();
         //Cwiczenie2Challenge.Run();
         //Cwiczenie3.Run();
-        Cwiczenie3Challenge.Run();
+        //Cwiczenie3Challenge.Run();
+        Cwiczenie4.Run();
     }
 }
